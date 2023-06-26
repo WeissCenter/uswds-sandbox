dc93dedc975dff85741438fdb4dfaf3f216e6792
Byron Stearns <bstearns@vizuri.com>
2023-06-26 12:31:09 -0400

    testing api stuff?

---
+++ packages/weiss-sandbox-azure-api/uswdsCompile/index.ts
@@ -0,0 +1,61 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+const sass = require('sass-embedded');
+import {parse} from 'postcss-scss';
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+
+};
+
+
+
+function checkForErrors(variable: string){
+
+    console.log("checking variable", variable)
+
+    const scss = 
+    `@use "uswds-core" with (
+        $theme-show-notifications: false,
+        $theme-show-compile-warnings: false,
+        ${variable}
+    );
+    @forward "uswds-theme";
+    @forward "uswds";
+    `
+
+    return sass.compileStringAsync(scss, {loadPaths: ['./uswds', './uswds/packages', './uswds/dist/theme'], style: 'compressed'})
+}
+
+
+async function compileSASS(variables: {[variable: string]: string}){
+    const sassVars = Object.entries(variables).map(([key, value]) => `${key}: (${value})`);
+
+    // await Promise.all(sassVars.map((vara) => checkForErrors(vara)))
+
+    const scss = 
+    `@use "uswds-core" with (
+        $theme-show-notifications: false,
+        $theme-show-compile-warnings: false,
+        ${sassVars.join(",\n")}
+    );
+    @forward "uswds-theme";
+    @forward "uswds";
+    `
+
+    return sass.compileStringAsync(scss, {loadPaths: ['../../node_modules/@uswds', '../../node_modules/@uswds/uswds/packages', '../../node_modules/@uswds/uswds/dist/theme'], style: 'compressed'})
+}
+
+
+
+
+
+export default httpTrigger;
